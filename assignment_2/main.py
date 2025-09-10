4305e9722d22ce1ae58923dfb84e458c801170fd
Guust207 <92439850+Guust207@users.noreply.github.com>
2025-09-10 14:59:55 +0200

    Added task 2 and 3

--- assignment_2/main.py
+++ assignment_2/main.py
@@ -81,3 +81,57 @@ def type_ii_feedback(observation, memory):
             elif observation[feature] == True:
                 memory.memorize_always('NOT ' + feature)
 
+# Task 2
+patients = [
+    # 1
+    {"lt40": False, "ge40": True, "premeno": False, "inv0-2": False, "inv3-5": True, "inv6-8": False, "deg1": False,
+     "deg2": False, "deg3": True, "rec": True},
+
+    # 2
+    {"lt40": True, "ge40": False, "premeno": False, "inv0-2": True, "inv3-5": False, "inv6-8": False, "deg1": False,
+     "deg2": False, "deg3": True, "rec": False},
+
+    # 3
+    {"lt40": False, "ge40": True, "premeno": False, "inv0-2": False, "inv3-5": False, "inv6-8": True, "deg1": False,
+     "deg2": False, "deg3": True, "rec": True},
+
+    # 4
+    {"lt40": False, "ge40": True, "premeno": False, "inv0-2": True, "inv3-5": False, "inv6-8": False, "deg1": False,
+     "deg2": True, "deg3": False, "rec": False},
+
+    # 5
+    {"lt40": False, "ge40": False, "premeno": True, "inv0-2": True, "inv3-5": False, "inv6-8": False, "deg1": False,
+     "deg2": False, "deg3": True, "rec": True},
+
+    # 6
+    {"lt40": False, "ge40": False, "premeno": True, "inv0-2": True, "inv3-5": False, "inv6-8": False, "deg1": True,
+     "deg2": False, "deg3": False, "rec": False}
+]
+
+# Rules:
+# R1: if Deg-malign 3 and not Menopause lt40 then Recurrence
+# R2: if Deg-malign 3 then Recurrence
+# R3: if Inv-nodes 0-2 then Non-Recurrence
+
+forget_value = 0.9
+memorize_value = 0.1
+
+# Task 3
+R1 = Memory(forget_value, memorize_value,
+            {"lt40": 5, "NOT lt40": 6, "ge40": 5, "NOT ge40": 5, "premeno": 5, "NOT premeno": 5, "inv0-2": 5,
+             "inv3-5": 5, "inv6-8": 5, "deg1": 5, "NOT deg1": 5, "deg2": 5, "NOT deg2": 5, "deg3": 6, "NOT deg3": 5}
+            )
+
+R2 = Memory(forget_value, memorize_value,
+            {"lt40": 5, "NOT lt40": 5, "ge40": 5, "NOT ge40": 5, "premeno": 5, "NOT premeno": 5, "inv0-2": 5,
+             "inv3-5": 5, "inv6-8": 5, "deg1": 5, "NOT deg1": 5, "deg2": 5, "NOT deg2": 5, "deg3": 6, "NOT deg3": 5}
+            )
+
+R3 = Memory(forget_value, memorize_value,
+            {"lt40": 5, "NOT lt40": 5, "ge40": 5, "NOT ge40": 5, "premeno": 5, "NOT premeno": 5, "inv0-2": 6,
+             "inv3-5": 5, "inv6-8": 5, "deg1": 5, "NOT deg1": 5, "deg2": 5, "NOT deg2": 5, "deg3": 5, "NOT deg3": 5}
+            )
+
+recurrence_rules = [R1, R2]
+non_recurrence_rules = [R3]
+
